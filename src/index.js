a42776367bd3b0472ff3b485e5a000243abc9e4e
Edward Timmer <edtimmer@gmail.com>
2019-06-13 20:57:13 -0400

    working on subscriptions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 import './styles/index.css'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker';
@@ -12,6 +15,16 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { BrowserRouter } from 'react-router-dom'
 import { setContext } from 'apollo-link-context'
 import { AUTH_TOKEN } from './constants'
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+})
 
 // 2
 const httpLink = createHttpLink({
@@ -28,9 +41,18 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 // 3
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 })
 
